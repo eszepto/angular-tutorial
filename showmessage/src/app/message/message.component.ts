1c226305b3b4dc57f3c55c156f903417430b6766
Pichet <pichetpump@gmail.com>
2021-06-18 14:48:42 +0700

    fix set value to message attribute at first

--- showmessage/src/app/message/message.component.ts
+++ showmessage/src/app/message/message.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-  message:String
+  message = ""
 
   constructor() { 
     this.message = ""
